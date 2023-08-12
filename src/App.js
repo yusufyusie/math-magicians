d533e282a412eb370b3aa103613ba07353ceee9c
yusufyusie <yesuf023@gmail.com>
2023-08-12 11:39:50 +0300

    Add category to quote

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Calculator />
-      <GetQuote />
+      <DisplayQuote />
     </>
   );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,39 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
-function GetQuote() {
-  const [data, setData] = useState([{ quote: '', author: '' }]);
-  const [hasError, setHasError] = useState(false);
+function DisplayQuote() {
+  const [data, setData] = useState([{ quote: '', author: '', category: '' }]);
+  const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const url = 'https://api.api-ninjas.com/v1/quotes';
+  const url = 'https://api.api-ninjas.com/v1/quotes?';
   const apiKey = 'z5XLf8fhX3+Ih1Uq/ImzfA==uzC71RdEcjGKYpXv';
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const res = await fetch('url', {
+        const response = await fetch(url, {
           method: 'GET',
-          headers: { 'X-Api-Key': 'apiKey' },
+          headers: { 'X-Api-Key': apiKey },
+          'Content-type': 'application/json; charset=UTF-8',
+          'access-control-allow-origin': '*',
+          'Access-Control-Allow-Credentials': 'true',
         });
-        const json = await res.json();
+        const json = await response.json();
         setData(json);
       } catch (error) {
-        setHasError(true);
+        setError(true);
       }
       setIsLoading(false);
     };
     fetchData();
   }, [setData, setIsLoading]);
 
-  if (hasError) return <div>Something went wrong!</div>;
-  if (isLoading) return <div>Loading...</div>;
+  if (error) return <p>Something went wrong!, can not display</p>;
+  if (isLoading) return <p>Loading... please wait</p>;
 
   return (
     <section id="quote">
       <q>{ data[0].quote }</q>
-      <p>{ data[0].author }</p>
+      <p><b>{ data[0].author }</b></p>
+      <q>{ data[0].category }</q>
     </section>
   );
 }
 
-export default GetQuote;
+export default DisplayQuote;
