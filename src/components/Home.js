3a0029a9f7d88d28b4c8ab540a173b01c01aa1c0
yusufyusie <yesuf023@gmail.com>
2023-08-15 16:53:45 +0300

    Add paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 function getParagraph() {
-    const str = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quasi laborum quisquam eum ipsam iusto, animi blanditiis aperiam nobis laudantium dolore quas cumque molestias,a perferendis corporis optio repudiandae quia ?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quos quidem iure illo suscipit voluptatum non at laudantium! Eum voluptatibus doloremque voluptate deleniti odio
-        natus qui ex tempore error, dignissimos nostrum!`;
+    const str = ` Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam voluptatibus
+    dolorum omnis? Optio facilis animi id saepe magni voluptatibus similique deserunt,
+    eum quae tempore, obcaecati, cupiditate officiis illum repellendus molestias aliquid
+    atque repudiandae. Assumenda eligendi doloribus nobis vel. Minima tempore necessitatibus
+    quae cumque deleniti totam ad vitae veniam in saepe! Lorem ipsum dolor, sit amet consectetur
+    adipisicing elit. Dolores at corrupti aut, nihil atque asperiores ex eaque. Nostrum qui
+    inventore maxime necessitatibus sit ad dolores doloremque laudantium unde voluptatibus.
+    Sed, ut iste! Quidem eveniet iste a porro enim ratione assumenda reprehenderit, quaerat facere
+    nostrum, iure, eos consequatur tenetur. Et, aperiam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, aspernatur! Blanditiis
+    dignissimos sed rerum, animi quia eligendi exercitationem laborum eius, culpa consequuntur
+    amet, at ratione! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit,
+    consequuntur iure impedit corporis, debitis, dolorum laborum nam ipsum voluptates ducimus
+    quisquam sapiente velit quas mollitia.`;
     return str;
   }
   
