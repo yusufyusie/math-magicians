dfced7a647b0a387113e6b701d9f32583817e449
yusufyusie <yesuf023@gmail.com>
2023-08-15 16:03:05 +0300

    update app component to support routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import DisplayQuote from './components/Quote';
+import Home from './components/Home';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <Calculator />
-      <DisplayQuote />
-    </>
+    <main>
+      <Navbar />
+      <div className="math-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<DisplayQuote />} />
+        </Routes>
+      </div>
+    </main>
   );
 }
 
